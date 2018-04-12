7f7d12ee157e220da7f610053e5da4ba618f3a98
xudabiao <xudabiao2010@gmail.com>
2018-04-12 18:24:20 +0800

    'ppp'

--- mmy/mmy/spiders/quotes_spider.py
+++ mmy/mmy/spiders/quotes_spider.py
@@ -5,14 +5,15 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com',
     ]
 
     def parse(self, response):
         for quote in response.css("div.quote"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
+                'text': quote.css("span.text::text").extract_first().strip(),
+                'author': quote.css("small.author::text").extract_first().strip(),
                 'tags': quote.css("div.tags a.tag::text").extract(),
             }
+        for a in response.css("li.next a"):
+            yield response.follow(a, callback=self.parse)
